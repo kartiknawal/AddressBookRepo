291fd31d98dafee11f568e7c46f84a93b599865e
kartiknawal <kartiknawal.7@gmail.com>
2020-10-03 07:20:38 +0530

    [kartiknawal] edit contact details

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace AddressBook
 {
     class Contacts
     {
-        string firstName, lastName, address, city, state, zipCode, phoneNo, eMail;
+        public string firstName, lastName, address, city, state, zipCode, phoneNo, eMail;
 
         public Contacts()
         {
@@ -44,6 +44,34 @@ namespace AddressBook
             Contacts contact = new Contacts(firstName, lastName, address, city, state, zipCode, phoneNo, eMail);
             contactList.Add(contact);
         }
+        public void editContact(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNo, string eMail)
+        {
+            foreach (Contacts c in contactList)
+            {
+                if (c.firstName.Equals(firstName))
+                {
+                    c.lastName = lastName;
+                    c.address = address;
+                    c.city = city;
+                    c.state = state;
+                    c.zipCode = zipCode;
+                    c.phoneNo = phoneNo;
+                    c.eMail = eMail;
+                    return;
+                }
+            }
+        }
+        public bool checkName(string firstName)
+        {
+            foreach (Contacts c in contactList)
+            {
+                if (c.firstName.Equals(firstName))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
     class Program
     {
@@ -52,16 +80,41 @@ namespace AddressBook
             AddressBook addressBook = new AddressBook();
             string[] details;
             Console.WriteLine("Welcome to Address Book Program");
+            while (true)
+            {
+                Console.WriteLine("1.Add Contact\n2.Edit Contact\n");
+                int choice = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the details");
-            Console.WriteLine("First Name, Last Name, Address, City, State, ZipCode, Email");
-            details = Console.ReadLine().Split(",");
+                switch (choice)
+                {
 
-            addressBook.addContact(details[0], details[1], details[2], details[3], details[4], details[5], details[6], details[7]);
+                    case 1:
+                        Console.WriteLine("Enter the details");
+                        Console.WriteLine("First Name, Last Name, Address, City, State, ZipCode, Email");
+                        details = Console.ReadLine().Split(",");
 
-            Console.WriteLine("Details Added");
+                        addressBook.addContact(details[0], details[1], details[2], details[3], details[4], details[5], details[6], details[7]);
 
-
+                        Console.WriteLine("Details Added");
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the name to edit");
+                        string name = Console.ReadLine();
+                        if (addressBook.checkName(name) == true)
+                        {
+                            Console.WriteLine("Enter the following details separated by comma");
+                            Console.WriteLine("Last Name, Address, City, State, ZipCode, Email");
+                            details = Console.ReadLine().Split(",");
+                            addressBook.editContact(name, details[0], details[1], details[2], details[3], details[4], details[5], details[6]);
+                            Console.WriteLine("Details editted successfully");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No such contact found");
+                        }
+                        break;
+                }
+            }
         }
     }
 }
